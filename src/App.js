02cb55f72215e59ec08df8791715120de3e45351
Robert Pliszka <robplisz@gmail.com>
2023-04-01 20:14:07 -0400

    Added one more item to the expense items

--- src/App.js
+++ src/App.js
@@ -21,6 +21,12 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: "e5",
+      title: "Car Payment",
+      amount: 650,
+      date: new Date(2021, 5, 12),
+    },
   ];
   return (
     <div>
@@ -29,6 +35,7 @@ function App() {
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
       <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <ExpenseItem title={expenses[4].title} amount={expenses[4].amount} date={expenses[4].date} />
     </div>
   );
 }
