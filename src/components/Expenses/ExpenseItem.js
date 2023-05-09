085f9bdf689c6d97afa7a9cb4522f15d8d963193
Robert Pliszka <robplisz@gmail.com>
2023-05-09 14:27:24 -0400

    Added another button to change the title back to original value

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -9,6 +9,10 @@ function ExpenseItem(props) {
   const clickHandler = () => {
     setTitle("Updated");
   };
+
+  const changeBackHandler = () => {
+    setTitle(props.title)
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -17,6 +21,7 @@ function ExpenseItem(props) {
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
+      <button onClick={changeBackHandler}>Change back</button>
     </Card>
   );
 }
