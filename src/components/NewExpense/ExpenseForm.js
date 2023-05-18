b271bc69702d8e6910608f67a6f39a8fd97a80bf
Robert Pliszka <robplisz@gmail.com>
2023-05-18 17:48:20 -0400

    Set functions to save entered values for title, price, and date to a state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,27 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  // Saving title to state
+  const [enteredTitle, setEnteredTitle] = useState("");
+
   const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    console.log(event.target.value);
+  };
+
+  // Saving price to state
+  const [enteredPrice, setEnteredPrice] = useState("");
+
+  const priceChangeHandler = (event) => {
+    setEnteredPrice(event.target.value);
+    console.log(event.target.value);
+  };
+
+  // Saving date to state
+  const [eneteredDate, setEnteredDate] = useState("");
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
     console.log(event.target.value);
   };
   return (
@@ -14,11 +34,21 @@ function ExpenseForm() {
         </div>
         <div className="new-expense__control">
           <label>Price</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={priceChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
