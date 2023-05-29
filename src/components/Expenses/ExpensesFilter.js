95ed397def480e7d4af4a110f6d3a8577194cc5e
Robert Pliszka <robplisz@gmail.com>
2023-05-28 21:14:33 -0400

    Created the filter component to choose the year in the Expenses component

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,16 +1,21 @@
-import react from "react";
+import React from "react";
 import "./ExpensesFilter.css";
 
-function ExpenseFilter() {
+function ExpenseFilter(props) {
+  // Passes the value from the dropdown menu lifting it to a parent componenet expenses
+  const setYearHandler = (event) => {
+    props.onAddFilterYear(event.target.value);
+  };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
-          <option vlaue="2023">2023</option>
-          <option vlaue="2022">2022</option>
-          <option vlaue="2021">2021</option>
-          <option vlaue="2020">2020</option>
+        <select value={props.selected} onChange={setYearHandler}>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
         </select>
       </div>
     </div>
