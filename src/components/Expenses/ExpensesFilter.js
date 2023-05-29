2f9544263740200955135e873760a66309bd568c
Robert Pliszka <robplisz@gmail.com>
2023-05-28 20:39:45 -0400

    Created the drop down for the expense filter component

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,6 +1,20 @@
 import react from "react";
 import "./ExpensesFilter.css";
 
-function ExpenseFilter() {}
+function ExpenseFilter() {
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select>
+          <option vlaue="2023">2023</option>
+          <option vlaue="2022">2022</option>
+          <option vlaue="2021">2021</option>
+          <option vlaue="2020">2020</option>
+        </select>
+      </div>
+    </div>
+  );
+}
 
 export default ExpenseFilter;
