14013725292128c711b922b4caf98a67c5cd45aa
Robert Pliszka <robplisz@gmail.com>
2023-06-04 17:03:33 -0400

    Added conditional statement to display filtered expenses and message if no expenses found for that year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,21 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  // Will display if no expenses are found
+  let expensesContent = <p>No expenses found.</p>;
+
+  // Checks if there are items in the array.
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -23,15 +38,8 @@ function Expenses(props) {
           selected={filteredYear}
           onAddFilterYear={saveFilterData}
         />
-        {/* Displays the expenses array filtered by year */}
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {/* Displays filtered expenses using conditional statement. If there are none it displays no expenses found */}
+        {expensesContent}
       </Card>
     </div>
   );
