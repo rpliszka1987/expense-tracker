6939cf1e0cebaa89babe4e4abf304ff9642c0059
Robert Pliszka <robplisz@gmail.com>
2023-06-04 20:18:17 -0400

    Fixed import of ExpensesList.css to effect styles

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css";
+import "./ExpensesList.css";
 
 function ExpensesList(props) {
   // Checks if there are items in the array.
