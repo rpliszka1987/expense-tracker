2fe1d6f42de8df77d4201850f62998a74a16281d
Robert Pliszka <robplisz@gmail.com>
2023-06-06 18:16:24 -0400

    Wrote the code for the chart component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,6 +1,21 @@
 import React from "react";
+import ChartBar from "./ChartBar";
 import "./Chart.css";
 
-function Chart() {}
+// Creates the chart to display the data on a chart
+function Chart(props) {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+}
 
 export default Chart;
